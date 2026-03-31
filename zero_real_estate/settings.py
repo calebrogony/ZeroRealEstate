b25f1371d705e165c14fee4ba169c2a3b3511928
Caleb <calebrogony92@gmail.com>
2026-03-31 21:46:23 +0300

    Add jazzmin and update ALLOWED_HOSTS

--- zero_real_estate/settings.py
+++ zero_real_estate/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['zerorealestate.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
